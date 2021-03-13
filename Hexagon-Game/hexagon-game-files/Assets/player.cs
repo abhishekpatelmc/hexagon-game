df332de1de9a5487f4ad0ea4a1bd660914c7f5b1
abhishekpatelmc <mcabhishekpatel2017@gmail.com>
2021-03-13 21:06:46 +0530

    updated

---
+++ Hexagon-Game/hexagon-game-files/Assets/player.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class player : MonoBehaviour
+{
+    //public float moveSpeed = 600f;
+
+    private Vector3 target = new Vector3(5.0f,0.0f,0.0f);
+    
+    //float movement = 0f;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        //movement = Input.GetAxisRaw("Horizontal");       
+    }
+
+    public void Pico()
+    {
+        //transform.RotateAround(Vector3.zero, Vector3.forward, movement * Time.fixedDeltaTime * -moveSpeed);
+        
+        transform.RotateAround(target, Vector3.up, 30 * Time.deltaTime);
+        Debug.Log("Hello");
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# hexagon
+
+Its a mobile game built from unity. the hexagons keep coming up we have to move the player to avoid for hexagons.
+Its a Cool game check it out....
+
+<img src="images/1.jpeg" width="205">       <img src="images/2.jpeg" width="205">
+<img src="images/3.jpeg" width="205">       <img src="images/4.jpeg" width="205">
